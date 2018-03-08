f7a5f84924ca9e49c2bb2ef5e39a0e70207d6c4a
TetroX <tet70x@gmail.com>
2018-03-08 20:55:07 +0500

    123

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// include jquery
+//= ../../bower_components/jquery/dist/jquery.js
+// include bootstrap.js
+//= ../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.js
+//= ../../bower_components/holderjs/holder.min.js
+
+console.log("i'm a live!");
+
+// another scripts
+// = partials/app.js
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIGluY2x1ZGUganF1ZXJ5XHJcbi8vPSAuLi8uLi9ib3dlcl9jb21wb25lbnRzL2pxdWVyeS9kaXN0L2pxdWVyeS5qc1xyXG4vLyBpbmNsdWRlIGJvb3RzdHJhcC5qc1xyXG4vLz0gLi4vLi4vYm93ZXJfY29tcG9uZW50cy9ib290c3RyYXAtc2Fzcy9hc3NldHMvamF2YXNjcmlwdHMvYm9vdHN0cmFwLmpzXHJcbi8vPSAuLi8uLi9ib3dlcl9jb21wb25lbnRzL2hvbGRlcmpzL2hvbGRlci5taW4uanNcclxuXHJcbmNvbnNvbGUubG9nKFwiaSdtIGEgbGl2ZSFcIik7XHJcblxyXG4vLyBhbm90aGVyIHNjcmlwdHNcclxuLy8gPSBwYXJ0aWFscy9hcHAuanMiXSwiZmlsZSI6Im1haW4uanMifQ==
